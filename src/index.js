d993e2bab989f3650a9eaf22b3aae208d62b424d
Karixkai <129597986+Karixkai@users.noreply.github.com>
2023-04-15 20:03:14 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,6 +17,59 @@ currentHours = ("0" + currentHours).slice(-2);
 let currentime = `${day} ${currentHours}:${currentminutes}`;
 weekday.innerHTML = currentime;
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+function getforecast(coordinates){
+  let apiKey = "3499ef150985eccadd080ff408a018df";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayforecast);
+
+}
+
+function displayforecast(response){
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = ``;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <li>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="weather-forecast-temperature-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </li>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+
 function displayWeatherCondition(response) {
    let descriptionElement = document.querySelector("#description");
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -35,6 +88,7 @@ cellink.addEventListener("click", celcius2);
   document.querySelector("#wind").innerHTML = Math.round(response.data.wind.speed);
   document.querySelector("#emoji").src = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
 celcius2(c);
+getforecast(response.data.coord);
 }
 
 function searchCity(city) {
